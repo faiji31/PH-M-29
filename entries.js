3ee01aa4124c025d2b271d0080d3058d28a7eca9
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-27 00:52:24 +0600

    11th commit

--- entries.js
+++ entries.js
@@ -9,5 +9,8 @@ const employee = {
 const keys = Object.keys(employee);
 
 const values = Object.values(employee);
+
+const entries = Object.entries(employee)
 console.log(keys)
 console.log(values)
+console.log(entries)
