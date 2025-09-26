276ae0c630bc8d8a38244beacac73d03e7b79da0
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-27 00:58:46 +0600

    13rd commit

--- freeze.js
+++ freeze.js
@@ -1,10 +1,13 @@
 const employee = {
     name: 'liam',
     designation: 'QA',
-    salary: '20000',
+    salary: 20000,
     experience: 3,
     age:22
 }
 
-delete employee.designation
+// delete employee.designation
+// delete employee.salary
+
+employee.salary = employee.salary + 2000;
 console.log(employee)
