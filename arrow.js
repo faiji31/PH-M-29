874ccf8b055c9b24d60925519321353b8c976c92
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-26 20:24:13 +0600

    3rd commoit

---
+++ arrow.js
@@ -0,0 +1,13 @@
+add(1,2);
+
+function add(num1,num2){
+    return num1+num2;
+}
+
+
+const addtion = function addition(n1,n2){
+    return n1 + n2 ;
+
+}
+const result = addtion(1,2);
+console.log(result)
