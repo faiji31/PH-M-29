4e7dc2dc419e172f1ae99deee9586c50266d6601
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-26 20:31:44 +0600

    4th commit

--- arrow.js
+++ arrow.js
@@ -11,3 +11,16 @@ const addtion = function addition(n1,n2){
 }
 const result = addtion(1,2);
 console.log(result)
+
+// arrow function
+const arr = (num1,num2) =>  num1 + num2
+
+const result1 = arr(2,5);
+console.log(result1)
+
+
+const arr2 = (n1,n2) => n1 * n2 
+
+
+const result2 = arr2(2,5)
+console.log(result2)
