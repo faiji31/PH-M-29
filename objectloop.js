4e175af1763958ab0c9b8272a9d1a6a42dad139b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-27 10:52:54 +0600

    17th commit

--- objectloop.js
+++ objectloop.js
@@ -1,4 +1,16 @@
 const num =[1,2,3,4,5];
 for(const i of num){
-    console.log(i);
+    // console.log(i);
+}
+
+employee ={
+    name:'liam',
+    Designation:'QA',
+    salary:20000,
+    age:25,
+    location:'barishal'
+}
+for (const j in employee){
+    const value = employee[j];
+    console.log(j,value);
 }
